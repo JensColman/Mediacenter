684f82fba8fb9f9cce09ca634345a0ffb5805da4
Jens Colman <colmanjens@gmail.com>
2019-07-07 11:43:05 +0200

    Masonry grid

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,81 @@
 const electron = require('electron');
 const { ipcRenderer } = electron;
-const ul = document.querySelector('#content')
+const row = document.querySelector('.row');
+const column = document.querySelectorAll('.column');
 
 ipcRenderer.on('ping', (event, message) => {
     console.log(message);
+    // console.log(message.length);
 
-    message.forEach((elem) => {
-        if (elem.match(/.(jpg|jpeg|png|gif|bmp)$/i)) {
-            ul.innerHTML += `<img class="element" src="${elem}">`;
-            ul.innerHTML += `<li class="source element">${elem}</li>`;
+        for (let i=0; i<message.length; i++) {
+            // if (message[i].match(/.(jpg|jpeg|png|gif|bmp)$/i)) {
+            //     console.log('image');
+            // } else if (message[i].match(/.(m4v|mpg|mp4|webm)$/i)) {
+            //     console.log('video');
+            // } else {
+            //     message.splice(i, 1);
+            //     console.log(message);
+            // }
+
+            if (!message[i].match(/.(jpg|jpeg|png|gif|bmp)$/i) || message[i].match(/.(m4v|mpg|mp4|webm)$/i)) {
+                message.splice(i, 1);
+                console.log(message);
+            }
+            
+        }
+        
+    let itemCounter = 0;
+
+    message.forEach((elem, index) => {
+
+        // switch (itemCounter) {
+        //     case 0:
+        //         placeContent(0);
+        //         break;
+
+        //     case 1:
+        //         placeContent(1);
+        //         break;
+
+        //     case 2:
+        //         placeContent(2);
+        //         break;
+                
+        //     case 3:
+        //         placeContent(3);
+        //         break;
+        // }
+
+        // const itemSwitch = (number) => ({
+        //     0: placeContent(0),
+        //     1: placeContent(1),
+        //     2: placeContent(2),
+        //     3: placeContent(3)
+        // });
+
+        // itemSwitch(itemCounter);
+
+        function placeContent(place) {
+            if (elem.match(/.(jpg|jpeg|png|gif|bmp)$/i)) {
+                column[place].innerHTML += `<img class="element" src="${elem}">`;
+                // row.innerHTML += `<li class="source element">${elem}</li>`;
+            }
+
+            if (elem.match(/.(m4v|mpg|mp4|webm)$/i)) {
+                column[place].innerHTML += `<video class="element" controls><source src="${elem}">Video is not supported.</video>`;
+                // row.innerHTML += `<li class="source element">${elem}</li>`;
+            }
+
+            // console.log(place);
+            if (place === 3) {
+                itemCounter = 0;
+            } else {
+                itemCounter++;
+            }
         }
 
-        if (elem.match(/.(m4v|mpg|mp4|webm)$/i)) {
-            ul.innerHTML += `<video class="element" controls><source src="${elem}">Video is not supported.</video>`;
-            ul.innerHTML += `<li class="source element">${elem}</li>`;
-        }
+        placeContent(itemCounter);
+
     });
 
 });
